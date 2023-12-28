f0b844fc075a33390a49dd65da7d1ea8903dfb27
waseemKashif <waseemkashif7@gmail.com>
2023-12-28 14:19:06 +0500

    analysed the app

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,31 +1,29 @@
+import React, { useState } from "react";
 
-import React, {useState} from "react";
- 
- const SearchBar =({onFormSubmitted})=>{
-        const [term,setTerm]=useState('');
+const SearchBar = ({ onFormSubmitted }) => {
+  const [term, setTerm] = useState("");
 
-        
-        const onSubmit =(e)=>{
-            onFormSubmitted(term);
-            e.preventDefault();
+  const onSubmit = (e) => {
+    onFormSubmitted(term);
+    e.preventDefault();
+  };
 
-        }
-        
-
-         return (
-           <div className="search-bar ui segment">
-             <form onSubmit={onSubmit} className="ui form">
-               <div className="field">
-                 <label>Search here</label>
-                 <input
-                   type="text"
-                   value={term}
-                   onChange={(e)=>{setTerm(e.target.value)}}
-                 />
-               </div>
-             </form>
-           </div>
-         );
- }
+  return (
+    <div className="search-bar ui segment">
+      <form onSubmit={onSubmit} className="ui form">
+        <div className="field">
+          <label>Search here</label>
+          <input
+            type="text"
+            value={term}
+            onChange={(e) => {
+              setTerm(e.target.value);
+            }}
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default SearchBar;
